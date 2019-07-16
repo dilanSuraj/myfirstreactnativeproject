717ff583996ac52e7e73f9320e57adf9c16113e9
dilanSuraj <suraj_amarasinghe@yahoo.com>
2019-07-16 22:41:17 +0530

    Input Completed

--- app/Main.js
+++ app/Main.js
@@ -3,18 +3,36 @@ import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header';
-
+import Input from './components/Input';
+const headerTitle = 'To Do';
 export default class Main extends React.Component{
 
+    state = {
+
+        inputValue: ''
+
+    };
+
+    newInputValue = value =>{
+
+        this.setState({
+            inputValue: value
+        })
+
+    };
     render() {
 
-        const headerTitle = 'To Do';
+
+        const {inputValue} = this.state;
 
         return(
             <LinearGradient colors = {primaryGradientArray} style = {styles.container}>
                 <StatusBar barStyle="light-content" />
                 <View style={styles.centered}>
                     <Header title={headerTitle}/>
+                </View>
+                <View style={styles.inputContainer}>
+                   <Input inputValue={inputValue} onChangeText={this.newInputValue}/>
                 </View>
             </LinearGradient>
         );
@@ -28,5 +46,9 @@ const styles = StyleSheet.create({
     },
     centered:{
         alignItems: 'center'
+    },
+    inputContainer: {
+        marginTop: 40,
+        paddingLeft: 15
     }
 });
